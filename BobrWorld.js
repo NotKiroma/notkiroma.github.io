74d041a2f6efd509f1637363744a0e660d78f7d2
NotKiroma <kiromachannel@gmail.com>
2024-09-30 19:40:44 +0300

    a

--- BobrWorld.js
+++ BobrWorld.js
@@ -5,3 +5,11 @@ let c = (a+b) * (b+a);
 
 console.log("Вот вам ваш пример!")
 console.log(c)
+
+const button = document.getElementById('button');
+button.addEventListener('click', function() {
+    if(button.className === 'coralbutton') {
+        button.className = 'corilbutton';
+    }
+    else {button.className = 'coralbutton';}
+});
