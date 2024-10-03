9481e0ba84bef9ac6f40a1f4ba62b55674f04340
NotKiroma <kiromachannel@gmail.com>
2024-10-03 16:52:29 +0300

    a

--- Lab 5/Button.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById('buttoncolor');
-
-button.addEventListener('click', function() {
-    if (button.className === 'coralbutton')
-    {button.className = 'lightcoralbutton';}
-    else
-    {button.className = 'coralbutton';}
-})
---
+++ Lab 5/para.js
@@ -0,0 +1,26 @@
+let num = 567;
+let first = num.toString();
+console.log(first[0]);
+
+let stroke = 'Alusha';
+console.log(stroke.length);
+
+let a = 'Voda';
+let b = 'Soda';
+let aa = (a[0])
+let bb = (b[0])
+if (aa === bb){
+    console.log("Первые буквы одинаковы")
+}else{
+    console.log("Первые буквы разные")
+}
+
+let vlad = 'Vitala';
+if (vlad.length > 1){
+    console.log(vlad.at(-1))
+} else {
+    console.log("NET")
+}
+
+
+
