ff56e22e9f9b33b93028132c9b88f23f58bd4467
Егор <kiromachannel@gmail.com>
2025-01-06 09:59:12 +0500

    Changes

--- Work_17/script.js
+++ Work_17/script.js
@@ -1,5 +1,7 @@
 let links = document.querySelectorAll("header a"),
     aaaa = new Map();
+const hashs = ["main", "about", "photos"];
+
 
 links.forEach(link => {
     link.addEventListener("click", async function (e) {
@@ -7,23 +9,34 @@ links.forEach(link => {
         let href = this.href;
         if(aaaa.has(href)) {
             document.querySelector("#container").innerHTML = aaaa.get(href);
+            window.location.hash = this.getAttribute("data-hash");
         }else{
             let data = await fetch(href);
             let text = await data.text();
             aaaa.set(href, text);
             document.querySelector("#container").innerHTML = text;
+            window.location.hash = this.getAttribute("data-hash");
         }
     });
 });
 
 async function start() {
-    let data = await fetch("main.html");
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    if (!hash || !hashs.includes(hash)){
+        hash = "main";
+    }
+    let data = await fetch(`${hash}.html`);
     let text = await data.text();
     aaaa.set("main.html", text);
     document.querySelector("#container").innerHTML = text;
     console.log(text);
 };
 start();
+
+window.addEventListener("hashchange", function () {
+    start();
+});
 
 
 
